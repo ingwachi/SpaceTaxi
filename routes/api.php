2bdc7fa36117db436f314189cfcd12f17c6142d7
Wachirawit Onchet <ingwachi@gmail.com>
2020-03-05 12:54:48 +0700

    RESTful API

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,15 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('/', function () {
+   return [
+       'version' => '1.0.0',
+       'name' => 'spacetaxi'
+   ];
 });
+
+Route::apiResource('/posts', 'Api\PostsController');
